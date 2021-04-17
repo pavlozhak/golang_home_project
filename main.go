5ea542449642f25cc27fdb2c0dc7ef8bc460d7fb
pavlo-zhak <zhak.pavlo@tftc.dev>
2021-04-17 15:50:52 +0300

    add simple server

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go serve is runing")
+}
+
+func about_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/about", about_page)
+	http.ListenAndServe(":4443", nil)
+}
+
 func main() {
-	fmt.Println("Go!")
+	handleRequest()
 }
